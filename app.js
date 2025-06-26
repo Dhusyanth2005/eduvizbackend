e43938460393c3b0636e21306c57563255762af9
Dhusyanth2005 <sdhusyanth20@gmail.com>
2025-06-26 16:03:03 +0530

    new

--- app.js
+++ app.js
@@ -23,13 +23,7 @@ const razorpayInstance = new Razorpay({
 initializeRazorpay(razorpayInstance);
 
 // CORS setup
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://eduviz-learning-platform.vercel.app',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'withCredentials'],
-  optionsSuccessStatus: 200
-}));
+app.use(cors('*'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
